4b89acaae1ba4543ad693ebab70b861c3159c6fa
rihem mersani <rihemmersani05@gmail.com>
2025-07-28 12:50:16 +0100

    fix: input interface error for vercel build

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,19 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => (
-    <input
-      ref={ref}
-      {...props}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm",
-        className
-      )}
-    />
-  )
-)
-Input.displayName = "Input"
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={`border border-gray-300 rounded-md px-3 py-2 ${className}`}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input };
